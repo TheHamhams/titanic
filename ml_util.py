86ca9b9712e5ae4ac39489e7d10241296de25d94
Corey Hamren <hamhams86@gmail.com>
2021-10-09 08:17:43 -0700

    created util file, used understanding funtion on dataset

---
+++ ml_util.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def understanding(df):
+    print(df.info())
+    print(df.describe())
+    for col in df.columns:
+        if df[col].dtype == 'int64' or df[col].dtype == 'float64':
+            plt.hist(df[col])
+            plt.title(col)
+            plt.show()
+            plt.clf()
+        elif df[col].dtype == 'object':
+            sns.barplot(df[col].value_counts().index,df[col].value_counts()).set_title(col)
+            plt.show()
